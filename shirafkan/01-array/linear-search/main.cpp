ece16dff0a5806f7397041cfcd0ea7134bd0bb88
Rezakntu <r.sh.khademi@gmail.com>
2025-11-03 06:57:45 +0330

    linear search

---
+++ shirafkan/01-array/linear-search/main.cpp
@@ -0,0 +1,37 @@
+// linear search
+#include <iostream>
+#include <conio.h>
+using namespace std;
+//////////////////////////////
+int find(int a[] , int n, int key)
+{
+    for(int i=0;i<=n-1 ; i++)
+        if (key==a[i])
+            return i;
+
+    return -1;
+}
+//////////////////////////////
+int main()
+{
+    int  a[]={17,8,13,20,6,14};
+
+    int n;
+    n=sizeof(a)/sizeof(a[0]);
+
+    int key=20;
+    int p;
+    p=find(a,n,key);
+
+    if(p==-1)
+        cout<<"not found";
+    else
+        cout<<p;
+
+
+
+
+
+    getch();
+    return 0;
+}
