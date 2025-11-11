ad842f358de34958fdab8240dd5b4e9a82db1d37
Rezakntu <r.sh.khademi@gmail.com>
2025-11-11 05:00:46 +0330

    convert to modern C++

--- shirafkan/04-list/linked-list-2/main.cpp
+++ shirafkan/04-list/linked-list-2/main.cpp
@@ -1,197 +1,120 @@
- //Linked List
-    #include <iostream>
-    #include <cstdio>
-    #include <cstdlib>
-    #include <conio.h>
-    using namespace std;
-/////////////////////////////////////////////////
-    struct node
-    {
-        int          data;
-        struct node  *next;
-    }*start;
+#include <iostream>
+using namespace std;
 
-/////////////////////////////////////////////////
-node *create_node(int val)
-    {
-        struct  node *n , *s;
+struct Node {
+    int data;
+    Node* next;
+};
 
-        n = new(struct node);
-        if (n == NULL)
-        {
-            cout<<"not allocated "<<endl;
-            return 0;
-        }
-        else
-        {
-            n->data = val;
-            n->next = NULL;
+Node* start = nullptr;
 
-            return n;
-        }
+Node* create_node(int val) {
+    Node* n = new Node();
+    n->data = val;
+    n->next = nullptr;
+    return n;
+}
+
+void insert_begin() {
+    int v;
+    cout << "Value: ";
+    cin >> v;
+
+    Node* n = create_node(v);
+    n->next = start;
+    start = n;
+}
+
+void insert_last() {
+    int v;
+    cout << "Value: ";
+    cin >> v;
+
+    Node* n = create_node(v);
+
+    if (start == nullptr) {
+        start = n;
+        return;
     }
 
-/////////////////////////////////////////////////
-    void insert_begin()
-    {
-        struct node *n, *p;
-        int v;
-        cout<<"value : ";
-        cin>>v;
-
-        n = create_node(v);
-
-        if (start == NULL)
-        {
-            start = n;
-            start->next = NULL;
-        }
-        else
-        {
-            p = start;
-            start = n;
-            start->next = p;
-        }
+    Node* s = start;
+    while (s->next != nullptr) {
+        s = s->next;
     }
 
-/////////////////////////////////////////////////
-    void insert_last()
-    {
-        struct node *n, *s;
-		int v;
-        cout<<"value : ";
-        cin>>v;
+    s->next = n;
+}
 
+void insert_pos() {
+    int v, pos;
+    cout << "Data: ";
+    cin >> v;
+    cout << "Position: ";
+    cin >> pos;
 
-        n = create_node(v);
+    Node* n = create_node(v);
 
-		s = start;
-        while (s->next != NULL)
-        {
-            s = s->next;
-        }
-
-        n->next = NULL;
-        s->next = n;
+    if (pos == 1) {
+        n->next = start;
+        start = n;
+        return;
     }
 
-/////////////////////////////////////////////////
-void insert_pos()
-    {
-        struct node *n, *s, *p;
-		int v, pos, c = 0;
-        cout<<"data : ";
-        cin>>v;
+    Node* s = start;
+    Node* p = nullptr;
+    int count = 1;
 
-
-        n = create_node(v);
-
-        cout<<"postion : ";
-        cin>>pos;
-
-        int i;
-        s = start;
-
-        while (s != NULL)
-        {
-            s = s->next;
-            c++;
-        }
-
-        if (pos == 1)
-        {
-            if (start == NULL)
-            {
-                start = n;
-                start->next = NULL;
-            }
-            else
-            {
-                p = start;
-                start = n;
-                start->next = p;
-            }
-        }
-        else if (pos > 1  && pos <= c)
-        {
-            s = start;
-            for (i = 1; i < pos; i++)
-            {
-                p = s;
-                s = s->next;
-            }
-            p->next = n;
-            n->next = s;
-        }
-        else
-        {
-            cout<<"Positon out of range"<<endl;
-        }
+    while (s != nullptr && count < pos) {
+        p = s;
+        s = s->next;
+        count++;
     }
-//////////////////////////////////////////////////////
- void display()
-    {
-        struct node *t;
-        if (start == NULL)
-        {
-            cout<<"Empty"<<endl;
-            return;
+
+    if (count == pos) {
+        p->next = n;
+        n->next = s;
+    } else {
+        cout << "Position out of range" << endl;
+        delete n;
+    }
+}
+
+void display() {
+    if (start == nullptr) {
+        cout << "Empty" << endl;
+        return;
+    }
+
+    Node* t = start;
+    cout << "Elements: ";
+    while (t != nullptr) {
+        cout << t->data << " -> ";
+        t = t->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    while (true) {
+        cout << "1. insert at begin\n";
+        cout << "2. insert at end\n";
+        cout << "3. insert at position\n";
+        cout << "4. show list\n";
+        cout << "5. exit\n";
+        cout << "Enter (1..5): ";
+
+        int x;
+        cin >> x;
+
+        switch (x) {
+            case 1: insert_begin(); break;
+            case 2: insert_last(); break;
+            case 3: insert_pos(); break;
+            case 4: display(); break;
+            case 5: return 0;
+            default: cout << "Invalid choice\n";
         }
 
-        t = start;
-        cout<<"Elements: "<<endl;
-
-		while (t != NULL)
-        {
-            cout << t->data << "->";
-            t = t->next;
-        }
-        cout<<"NULL"<<endl;
-   }
-//////////////////////////////////////////////////
-main()
- {
-   int x;
-   start=NULL;
-
-   while(1)
-   {
-   	cout<<"1. insert in begin     " << endl;
-   	cout<<"2. insert in end       " << endl;
-   	cout<<"3. insert in position  " << endl;
-   	cout<<"4. show list           " << endl;
-   	cout<<"5. exit                " << endl;
-   	cout<<"enter(1..5):           " << endl;
-
-	cin >>x;
-
-	switch(x)
-	{
-		case 1:
-			    insert_begin();
-			    cout<<endl;
-			    break;
-		case 2:
-			    insert_last();
-			    cout<<endl;
-			    break;
-		case 3:
-			    insert_pos();
-			    cout<<endl;
-			    break;
-		case 4:
-			    display();
-			    cout<<endl;
-			    break;
-		case 5:
-			    exit(1);
-			    break;
-	}
-
-   }
-
-
-
-
-    getch();
- }
+        cout << endl;
+    }
+}
