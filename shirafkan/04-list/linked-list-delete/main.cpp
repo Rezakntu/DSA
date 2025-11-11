a4fdd2fe4be148113dabe69ffef939306e68bd60
Rezakntu <r.sh.khademi@gmail.com>
2025-11-11 05:44:10 +0330

    convert to modern C++ ,and rewrite it cleaner

--- shirafkan/04-list/linked-list-delete/main.cpp
+++ shirafkan/04-list/linked-list-delete/main.cpp
@@ -1,150 +1,104 @@
-//delete
 #include <iostream>
-#include <cstdlib>
-#include <conio.h>
 using namespace std;
-/////////////////////////////
-struct node
-    {
-        int          data;
-        struct node  *next;
-    }*start;
-/////////////////////////////
-node *create_node(int v)
-    {
-        struct  node *n , *s;
 
-        n = new(struct node);
-        if (n == NULL)
-        {
-            cout<<"not allocated "<<endl;
-            return 0;
-        }
-        else
-        {
-            n->data = v;
-            n->next = NULL;
-            return n;
-        }
-    }
+struct Node {
+    int data;
+    Node* next;
+};
 
-////////////////////////////////
-void insert_begin()
-    {
-        struct node *n, *p;
-		int v;
-        cout<<"data: ";
-        cin>>v;
+Node* start = nullptr;
 
-
-        n = create_node(v);
-
-        if (start == NULL)
-        {
-            start = n;
-            start->next = NULL;
-        }
-        else
-        {
-            p = start;
-            start = n;
-            start->next = p;
-        }
-    }
-
-///////////////////////////////////
-void del_pos()
-    {
-        struct node  *s ,*p;
-		int pos,i,c=0;
-
-        if (start == NULL) return;
-
-		cout << "position: ";
-        cin  >> pos;
-
-        s = start;
-        if (pos == 1)
-		     start = s->next;
-        else{
-            while (s != NULL)
-			{
-                s = s->next;
-                c++;
-            }
-
-            if (pos>=2 && pos<=c)
-			{
-                s = start;
-                for (i=1; i<=pos-1; i++)
-				{
-                    p = s;
-                    s = s->next;
-                }
-                p->next = s->next;
-            }
-            else
-                cout<<"out of range"<<endl;
-            free(s);
-        }
+Node* create_node(int v) {
+    Node* n = new Node();
+    n->data = v;
+    n->next = nullptr;
+    return n;
 }
 
-///////////////////////////////////
-void show()
-    {
-        struct node *n;
-        if (start == NULL)
-        {
-            cout<<"Empty"<<endl;
-            return;
-        }
+void insert_begin() {
+    int v;
+    cout << "Data: ";
+    cin >> v;
 
-        n = start;
-        cout<<"Elements: "<<endl;
-        while (n != NULL)
-        {
-            cout<<n->data<<"->";
-            n = n->next;
-        }
-        cout<<"NULL"<<endl;
+    Node* n = create_node(v);
+    n->next = start;
+    start = n;
+}
+
+void del_pos() {
+    if (start == nullptr) {
+        cout << "List empty" << endl;
+        return;
     }
-/////////////////////////////////////
-main()
-    {
-        int   x;
-        start = NULL;
 
-        while (1)
-        {
-            cout <<"1 . Insert at beginning "  << endl;
-            cout <<"2 . delete              " << endl;
-            cout <<"3 . Display             " << endl;
-            cout <<"4 . Exit                " << endl;
-            cout <<"Enter (1 or 2 or 3 or 4) : ";
+    int pos;
+    cout << "Position: ";
+    cin >> pos;
 
-            cin>>x;
-            switch(x)
-            {
-            case 1:
-                insert_begin();
-                cout<<endl;
-                break;
+    if (pos < 1) {
+        cout << "Invalid" << endl;
+        return;
+    }
 
-            case 2:
-                del_pos();
-                break;
+    Node* s = start;
+    Node* p = nullptr;
 
-            case 3:
-                show();
-                cout<<endl;
-                break;
+    if (pos == 1) {
+        start = start->next;
+        delete s;
+        return;
+    }
 
-            case 4:
-                exit(1);
-                break;
+    int count = 1;
+    while (s != nullptr && count < pos) {
+        p = s;
+        s = s->next;
+        count++;
+    }
 
-            }
+    if (s == nullptr) {
+        cout << "Out of range" << endl;
+        return;
+    }
+
+    p->next = s->next;
+    delete s;
+}
+
+void show() {
+    if (start == nullptr) {
+        cout << "Empty" << endl;
+        return;
+    }
+
+    Node* n = start;
+    cout << "Elements: ";
+    while (n != nullptr) {
+        cout << n->data << " -> ";
+        n = n->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    while (true) {
+        cout << "1. Insert at beginning\n";
+        cout << "2. Delete position\n";
+        cout << "3. Display\n";
+        cout << "4. Exit\n";
+        cout << "Enter (1-4): ";
+
+        int x;
+        cin >> x;
+
+        switch (x) {
+            case 1: insert_begin(); break;
+            case 2: del_pos(); break;
+            case 3: show(); break;
+            case 4: return 0;
+            default: cout << "Invalid choice" << endl;
         }
-    getch();
- }
 
+        cout << endl;
+    }
+}
