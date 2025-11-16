d2c269c64555e210b923745e51c75db58ed81f6a
Reza <r.sh.khademi@gmail.com>
2025-11-16 12:51:28 +0330

    Converted to modern C++

--- shirafkan/05-DList/double-linkedlist-reverse/main.cpp
+++ shirafkan/05-DList/double-linkedlist-reverse/main.cpp
@@ -1,34 +1,31 @@
-//reverse : Doubly Linked List
-#include<iostream>
-#include<cstdio>
-#include<conio.h>
-
+#include <iostream>
 using namespace std;
+
 ///////////////////////////////////////
-struct node
+struct Node
 {
     int data;
-    struct node *next;
-    struct node *prev;
-}*start;
+    Node* next;
+    Node* prev;
+};
+
+Node* start = nullptr;
 
 /////////////////////////////////////////////////////////
 void create(int item)
 {
-    struct node *s, *t;
-    t = new(struct node);
+    Node* t = new Node;
     t->data = item;
-    t->next = NULL;
-    if (start == NULL)
-    {
-        t->prev = NULL;
+    t->next = nullptr;
+    t->prev = nullptr;
+
+    if (start == nullptr) {
         start = t;
-    }
-    else
-    {
-        s = start;
-        while (s->next != NULL)
+    } else {
+        Node* s = start;
+        while (s->next != nullptr)
             s = s->next;
+
         s->next = t;
         t->prev = s;
     }
@@ -37,55 +34,55 @@ void create(int item)
 /////////////////////////////////////////////////////////
 void show()
 {
-    struct node *q;
-    if (start == NULL)
-    {
-        cout<<"empty"<<endl;
+    if (start == nullptr) {
+        cout << "empty\n";
         return;
     }
 
-    q = start;
-    while (q != NULL)
-    {
-        cout<<q->data<<" <-> ";
+    Node* q = start;
+    while (q != nullptr) {
+        cout << q->data << " <-> ";
         q = q->next;
     }
-    cout<<"NULL"<<endl;
+    cout << "NULL\n";
 }
+
 /////////////////////////////////////////////////////////
-void reverse()
+void reverseList()
 {
-    struct node *p1, *p2;
+    if (start == nullptr || start->next == nullptr)
+        return;
 
-    p1 = start;
-    p2 = p1->next;
+    Node* p1 = start;
+    Node* p2 = p1->next;
 
-    p1->next = NULL;
+    p1->next = nullptr;
     p1->prev = p2;
 
-    while (p2 != NULL)
+    while (p2 != nullptr)
     {
         p2->prev = p2->next;
         p2->next = p1;
+
         p1 = p2;
         p2 = p2->prev;
     }
-    start = p1;
 
+    start = p1;
 }
-///////////////////
+
+/////////////////////////////////////////////////////////
 int main()
 {
     create(4);
     create(7);
     create(2);
 
-    show();
+    show();         // 4 <-> 7 <-> 2 <-> NULL
 
-    reverse();
+    reverseList();
 
-    show();
+    show();         // 2 <-> 7 <-> 4 <-> NULL
 
-
-    getch();
+    return 0;
 }
