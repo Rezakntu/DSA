a0c0c60d323e9f17635e429eb369e577a9f2aafd
Rezakntu <r.sh.khademi@gmail.com>
2026-06-24 06:54:29 +0330

    a simple example for hash set

---
+++ Tanha/01/09-transposeSparse/cmake-build-debug/CMakeFiles/09_transposeSparse.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "09_transposeSparse.exe"
+  "09_transposeSparse.exe.manifest"
+  "09_transposeSparse.pdb"
+  "CMakeFiles/09_transposeSparse.dir/main.cpp.obj"
+  "CMakeFiles/09_transposeSparse.dir/main.cpp.obj.d"
+  "lib09_transposeSparse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/09_transposeSparse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
