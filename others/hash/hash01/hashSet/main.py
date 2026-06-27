ee7176bee904c94c6b64762bc26dbd7e3f983482
Rezakntu <r.sh.khademi@gmail.com>
2026-06-27 04:54:42 +0330

    project dependencies modified

--- others/hash/hash01/hashSet/main.py
+++ others/hash/hash01/hashSet/main.py
@@ -1,14 +1,11 @@
 my_array = ['Pete', 'Jones', 'Lisa', 'Bob', 'Siri']
-
 # create 10 buckets
 my_hash_set = [[] for _ in range(10)]
-
 def hash_function(value):
     sum_of_chars = 0
     for char in value:
         sum_of_chars += ord(char)
     return sum_of_chars % 10
-
 
 def add(value):
     index = hash_function(value)
@@ -17,19 +14,16 @@ def add(value):
     if value not in bucket:
         bucket.append(value)
 
-
 def contains(value):
     index = hash_function(value)
     bucket = my_hash_set[index]
     return value in bucket
-
 
 # insert values
 for name in my_array:
     add(name)
 
 print(my_hash_set)
-
 print("'Bob' has hash code:", hash_function('Bob'))
 print("'Pete' is in the Hash Set:", contains('Pete'))
 
